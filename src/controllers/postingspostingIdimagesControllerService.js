98559cf67ecc688967461522707b68d94863e1f5
ascheipers <andreas.scheipers@stud.h-da.de>
2021-02-23 16:21:42 +0200

    Move storeImage function to new location

--- src/controllers/postingspostingIdimagesControllerService.js
+++ src/controllers/postingspostingIdimagesControllerService.js
@@ -1,6 +1,24 @@
 'use strict'
+
 const path = require('path');
-const { storeImage } = require('../util/images')
+const fs = require('fs');
+const models = require('../../models');
+
+async function storeImage(buffer, location, posting) {
+  // TODO: verify is image
+
+  const dbImage = models.Image.build({ posting });
+
+  await fs.writeFile(path.join(location, dbImage.id + '.jpg'), buffer, { flag: 'wx' }, err => {
+    if (err) {
+      throw err;
+    }
+  });
+
+  await dbImage.save();
+
+  return dbImage.id;
+}
 
 module.exports.funcpostingspostingIdimagesPARAMETERS = function funcpostingspostingIdimagesPARAMETERS(req, res, next) {
   res.send({
