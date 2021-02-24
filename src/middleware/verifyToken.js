2bd5807f1fa76aea1f1f3fc9631b1dfc6697f17f
ascheipers <andreas.scheipers@stud.h-da.de>
2021-02-24 14:03:29 +0200

    Add middleware to check if logged in

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../util/settings');
+
+function verifyToken (req, res, next) {
+  try {
+    let token = req.headers.authorization;
+
+    if (!token) {
+      throw 'No authorization token provided.';
+    }
+
+    token = token.split(' ')[1];
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        throw 'Unauthorized.';
+      }
+      req.userId = decoded.id;
+      next(req, res, next);
+    });
+  } catch (error) {
+    return res.status(400).send({ errorCode: 'E400', errorMessage: error });
+  }
+};
+
+module.exports = verifyToken;
