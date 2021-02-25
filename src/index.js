cc15ccb95f76bcf2a7d1e6f6a31c9acd6a477bda
Andreas Scheipers <andreas.scheipers@stud.h-da.de>
2021-02-26 01:39:22 +0200

    Fix port for use on heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const { HOST_PORT } = require('./util/settings');
 
 async function startServer() {
   const app = await expressApp();
-  http.createServer(app).listen(HOST_PORT, () => {
+  http.createServer(app).listen(process.env.PORT || HOST_PORT, () => {
     console.log("App running at http://localhost:" + HOST_PORT);
     console.log("________________________________________________________________");
     console.log('API docs (Swagger UI) available on http://localhost:' + HOST_PORT + '/docs');
